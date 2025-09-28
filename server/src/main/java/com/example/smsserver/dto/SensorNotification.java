b1560ac3cedab4bf0e8cd623758ee71c2cf178a9
Steven <pcmrsteven@gmail.com>
2025-09-28 20:00:58 +1000

    add comments to SensorNotification class for documentation

--- server/src/main/java/com/example/smsserver/dto/SensorNotification.java
+++ server/src/main/java/com/example/smsserver/dto/SensorNotification.java
@@ -6,6 +6,8 @@ import lombok.Data;
 
 @Data
 @Builder
+// SensorNotification is a POJO class which represents the JSON request the Pico device will
+// send to the server to alert that motion has been detected
 public class SensorNotification {
     @NotBlank
     private String sensorID;
