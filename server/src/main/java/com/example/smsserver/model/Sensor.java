41cfa4c619310fc3c296324998c2db943e1f3c4f
Steven <pcmrsteven@gmail.com>
2025-09-28 20:00:45 +1000

    create Sensor POJO class

--- server/src/main/java/com/example/smsserver/model/Sensor.java
+++ server/src/main/java/com/example/smsserver/model/Sensor.java
@@ -1,4 +1,23 @@
 package com.example.smsserver.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "sensors")
+
+// This is an Entity class which represents the Pico Device
 public class Sensor {
+
+    @Id
+    @NotBlank
+    @NotNull
+    @Column(unique = true, nullable = false)
+    private String id;
 }
