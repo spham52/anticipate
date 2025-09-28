52221a98d67a3986d5daaf255b11c1d69e1ea928
Steven <pcmrsteven@gmail.com>
2025-09-28 20:02:11 +1000

    - add additional validation checks for User entity class - add comments for documentation

--- server/src/main/java/com/example/smsserver/model/User.java
+++ server/src/main/java/com/example/smsserver/model/User.java
@@ -4,7 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+
+import java.util.UUID;
 
 @Entity
 @NoArgsConstructor
@@ -13,17 +19,29 @@ import lombok.*;
 @Getter
 @Setter
 @Table(name= "users" )
+
+// Entity class that represents the application user
 public class User {
     @Id
-    private String userID;
+    @Builder.Default
+    private String userID = UUID.randomUUID().toString();
 
     @Column(unique=true, nullable = false)
+    @NotBlank
+    @NotNull
+    @Size(min = 8, max = 24)
     private String username;
 
     @Column(nullable = false)
+    @NotBlank
+    @NotNull
+    @Size(min = 8, max = 36)
     private String password;
 
     @Column(unique = true, nullable = false)
+    @NotBlank
+    @NotNull
+    @Email
     private String email;
 
     @Override
