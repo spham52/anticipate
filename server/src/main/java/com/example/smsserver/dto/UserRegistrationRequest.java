3b1959b32c316ae036f55863676486fd2975b53f
Steven <pcmrsteven@gmail.com>
2025-09-28 20:03:16 +1000

    add UserRegistrationRequest POJO

---
+++ server/src/main/java/com/example/smsserver/dto/UserRegistrationRequest.java
@@ -0,0 +1,28 @@
+package com.example.smsserver.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UserRegistrationRequest {
+
+    @NotBlank
+    @NotNull
+    @Size(min = 8, max = 20)
+    String username;
+
+    @NotBlank
+    @NotNull
+    @Size(min = 8, max = 36)
+    String password;
+
+    @NotBlank
+    @NotNull
+    @Email
+    String email;
+}
