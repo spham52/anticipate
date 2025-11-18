7ce3c4bee18cde3f76aa7bcb801d3540b653c223
Steven <pcmrsteven@gmail.com>
2025-11-18 14:16:07 +1100

    - add comment mentioning that sensorID is the primary key when using UserSensor

--- server/src/main/java/com/example/smsserver/repository/UserSensorRepository.java
+++ server/src/main/java/com/example/smsserver/repository/UserSensorRepository.java
@@ -3,5 +3,6 @@ package com.example.smsserver.repository;
 import com.example.smsserver.model.UserSensor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// SensorID is primary key
 public interface UserSensorRepository extends JpaRepository<UserSensor, String> {
 }
