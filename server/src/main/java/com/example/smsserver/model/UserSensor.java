65bfa7081c43aa3d9457516bf1c48f9db559be86
Steven <pcmrsteven@gmail.com>
2025-11-18 14:15:39 +1100

    - add builder annotation to UserSensor

--- server/src/main/java/com/example/smsserver/model/UserSensor.java
+++ server/src/main/java/com/example/smsserver/model/UserSensor.java
@@ -2,16 +2,17 @@ package com.example.smsserver.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.IdClass;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "user_sensor")
 
