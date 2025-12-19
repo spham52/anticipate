e924649f355e511e5c3df202091b4f8237c23f50
Steven <pcmrsteven@gmail.com>
2025-12-19 17:03:15 +1100

    - wrap Routes in AuthProvider component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,20 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Root from "./views/Root.jsx";
 import SignUp from "./views/SignUp.jsx";
 import Login from "./views/Login.jsx";
+import {AuthProvider} from "./firebase/AuthProvider";
+import Dashboard from "./views/Dashboard";
 
 function App() {
   return (
       <Router>
+          <AuthProvider>
           <Routes>
               <Route path="/" element={<Root/>}></Route>
               <Route path="/login" element={<Login/>}></Route>
               <Route path="/signup" element={<SignUp/>}></Route>
+              <Route path="/dashboard" element={<Dashboard/>}></Route>
           </Routes>
+          </AuthProvider>
       </Router>
   );
 }
