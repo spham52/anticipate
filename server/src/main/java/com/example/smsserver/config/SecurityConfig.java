e59adff432781bc6973956644da9480a1c53f1ff
Steven <pcmrsteven@gmail.com>
2025-12-19 16:47:15 +1100

    - add authentication to all endpoints except for whitelisted endpoints such as /user/register

--- server/src/main/java/com/example/smsserver/config/SecurityConfig.java
+++ server/src/main/java/com/example/smsserver/config/SecurityConfig.java
@@ -1,15 +1,39 @@
 package com.example.smsserver.config;
 
+import com.example.smsserver.filter.TokenAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    // end points that can be accessed without authentication
+    private static final String[] WHITELISED_API_ENDPOINTS = {"/user/register"};
+    private final TokenAuthenticationFilter tokenAuthenticationFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-     return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authManager -> {
+                    // allow unauthorised access to WHITELISTED_API_ENDPOINTS
+                    authManager.requestMatchers(HttpMethod.POST, WHITELISED_API_ENDPOINTS)
+                            .permitAll()
+                            .anyRequest() // else other endpoints have to be authenticated
+                            .authenticated();
+                })
+                .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 }
