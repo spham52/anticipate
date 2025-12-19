900100e2d54cee5c0569ec1abd1586b75c42f79d
Steven <pcmrsteven@gmail.com>
2025-12-19 16:47:36 +1100

    - add function to find sensor by user

--- server/src/main/java/com/example/smsserver/repository/SensorRepository.java
+++ server/src/main/java/com/example/smsserver/repository/SensorRepository.java
@@ -1,7 +1,11 @@
 package com.example.smsserver.repository;
 
 import com.example.smsserver.model.Sensor;
+import com.example.smsserver.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SensorRepository extends JpaRepository<Sensor, String> {
+    List<Sensor> findSensorByUser(User user);
 }
