923a667e59f45cc03637d2dc9ff715f28feaae89
Steven <pcmrsteven@gmail.com>
2025-12-19 17:05:19 +1100

    - Add Firebase Auth to client and fix env variable prefixes

--- frontend/src/firebase/Firebase.jsx
+++ frontend/src/firebase/Firebase.jsx
@@ -1,17 +1,19 @@
 import {initializeApp} from "firebase/app";
 import {getMessaging, getToken, onMessage} from "firebase/messaging";
+import { getAuth } from "firebase/auth";
 
 export const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 }
 
 export const FIREBASE_VAPID_KEY = process.env.FIREBASE_FIREBASE_VAPID_KEY;
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 const messaging = getMessaging(app);
 
 export const requestForToken = () => {
@@ -36,3 +38,7 @@ onMessage(messaging, ({notification}) => {
         icon: notification.icon,
     });
 });
+
+export const onLogout = (e) => {
+    auth.signOut();
+}
