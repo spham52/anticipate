9c01371c7790880ebfa8eed10e93f0b5bf1cffca
Steven <pcmrsteven@gmail.com>
2025-12-19 16:55:28 +1100

    - create TokenRegistrationServiceImpl test class

---
+++ server/src/test/java/com/example/smsserver/service/TokenRegistrationServiceImplTest.java
@@ -0,0 +1,79 @@
+package com.example.smsserver.service;
+
+import com.example.smsserver.dto.TokenRegistrationRequestDTO;
+import com.example.smsserver.exception.UserAlreadyExistsException;
+import com.example.smsserver.model.TokenRegistration;
+import com.example.smsserver.repository.TokenRegistrationRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TokenRegistrationServiceImplTest {
+
+    @Mock
+    TokenRegistrationRepository tokenRegistrationRepository;
+
+    @InjectMocks
+    TokenRegistrationServiceImpl tokenRegistrationService;
+
+    @Test
+    void registerToken_throwsUserAlreadyExistsException_whenUserExists() {
+        String userID = "user-123";
+        String tokenID = "token-123";
+        String platform = "android";
+        String appVersion = "1.0.0";
+
+        TokenRegistrationRequestDTO request = TokenRegistrationRequestDTO
+                .builder()
+                .tokenID(tokenID)
+                .platform(platform)
+                .appVersion(appVersion)
+                .build();
+
+        when(tokenRegistrationRepository.existsByUserID(userID)).thenReturn(true);
+        assertThrows(UserAlreadyExistsException.class, () -> {
+            tokenRegistrationService.registerToken(request, userID);
+        });
+    }
+
+    @Test
+    void registerToken_whenValidTokenRequest() {
+        String userID = "user-123";
+        String tokenID = "token-123";
+        String platform = "android";
+        String appVersion = "1.0.0";
+
+        TokenRegistrationRequestDTO request = TokenRegistrationRequestDTO.builder()
+                .tokenID(tokenID)
+                .platform(platform)
+                .appVersion(appVersion)
+                .build();
+
+        when(tokenRegistrationRepository.existsByUserID(userID)).thenReturn(false);
+
+        TokenRegistration saved = TokenRegistration.builder()
+                .userID(userID)
+                .tokenID(tokenID)
+                .platform(platform)
+                .appVersion(appVersion)
+                .build();
+
+        when(tokenRegistrationRepository.save(any(TokenRegistration.class))).thenReturn(saved);
+
+        TokenRegistration result = tokenRegistrationService.registerToken(request, userID);
+
+        verify(tokenRegistrationRepository).existsByUserID(userID);
+        verify(tokenRegistrationRepository).save(any(TokenRegistration.class));
+
+        assertEquals(userID, result.getUserID());
+        assertEquals(tokenID, result.getTokenID());
+    }
+}
