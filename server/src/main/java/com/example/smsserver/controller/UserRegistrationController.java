cff27298ca9b4431d3f2a96a71c51fa78e95c84d
Steven <pcmrsteven@gmail.com>
2025-12-19 16:57:35 +1100

    - refactor UserRegistrationController endpoints

--- server/src/main/java/com/example/smsserver/controller/UserRegistrationController.java
+++ server/src/main/java/com/example/smsserver/controller/UserRegistrationController.java
@@ -1,8 +1,6 @@
 package com.example.smsserver.controller;
 
-import com.example.smsserver.dto.UserRegistrationRequest;
-import com.example.smsserver.model.User;
-import com.example.smsserver.repository.UserRepository;
+import com.example.smsserver.dto.UserRegistrationRequestDTO;
 import com.example.smsserver.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -11,14 +9,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/user")
 @RequiredArgsConstructor
 // REST Controller class for handling user registration requests
 public class UserRegistrationController {
     private final UserService userService;
 
-    @PostMapping("/user/register")
-    public ResponseEntity<String> registerUser(@RequestBody @Valid UserRegistrationRequest userRegistration) {
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody @Valid UserRegistrationRequestDTO userRegistration) {
         userService.registerUser(userRegistration);
         return ResponseEntity.ok("User registered successfully");
     }
