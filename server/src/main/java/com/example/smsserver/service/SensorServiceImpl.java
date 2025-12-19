0d8f6582f64634195ee3bab9389e0219eaac46e7
Steven <pcmrsteven@gmail.com>
2025-12-19 16:50:42 +1100

    SensorService - add function to retrieve notifications by sensor - add function to save notifications to db - add function to find sensor by sensor ID (throws SensorDoesNotExistException if not found) - add function to find sensor by user object

--- server/src/main/java/com/example/smsserver/service/SensorServiceImpl.java
+++ server/src/main/java/com/example/smsserver/service/SensorServiceImpl.java
@@ -1,25 +1,31 @@
 package com.example.smsserver.service;
 
-import com.example.smsserver.dto.SensorRegistrationRequest;
+import com.example.smsserver.dto.SensorNotificationDTO;
+import com.example.smsserver.dto.SensorRegistrationRequestDTO;
 import com.example.smsserver.exception.SensorAlreadyAssociatedWithUserException;
 import com.example.smsserver.exception.SensorDoesNotExistException;
+import com.example.smsserver.exception.UnauthorisedAccessException;
 import com.example.smsserver.model.Sensor;
+import com.example.smsserver.model.SensorNotification;
 import com.example.smsserver.model.User;
+import com.example.smsserver.repository.SensorNotificationRepository;
 import com.example.smsserver.repository.SensorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class SensorServiceImpl implements SensorService {
     private final SensorRepository sensorRepository;
+    private final SensorNotificationRepository sensorNotificationRepository;
     private final UserService userService;
 
     @Override
-    public void associateUserWithSensor(SensorRegistrationRequest request) {
-        // find sensor, throw exception if not found
-        Sensor sensor = sensorRepository.findById(request.getSensorID()).orElseThrow(() ->
-                new SensorDoesNotExistException(request.getSensorID()));
+    public void associateUserWithSensor(SensorRegistrationRequestDTO request, String userID) {
+        Sensor sensor = findSensorById(request.getSensorID());
 
         // if user already associated with sensor, throw exception
         if (sensor.getUser() != null) {
@@ -27,8 +33,42 @@ public class SensorServiceImpl implements SensorService {
         }
 
         // find user associated with userID in request
-        User user = userService.getUserById(request.getUserID());
+        User user = userService.findUserById(userID);
         sensor.setUser(user);
         sensorRepository.save(sensor);
+    }
+
+    @Override
+    public List<SensorNotification> findAllNotificationsBySensor(Sensor sensor, String userID) {
+        if (!sensor.getUser().getUserID().equals(userID)) {
+            throw new UnauthorisedAccessException("You do not own this sensor");
+        }
+
+        // add token verification
+        return sensorNotificationRepository.
+                findSensorNotificationBySensorIdOrderByTimestampDesc(sensor.getId());
+    }
+
+    @Override
+    public void saveNotification(SensorNotificationDTO notification) {
+        LocalDateTime time = LocalDateTime.now();
+        Sensor sensor = findSensorById(notification.getSensorID());
+        SensorNotification sensorNotification = SensorNotification.builder()
+                .sensor(sensor)
+                .timestamp(time)
+                .build();
+        sensorNotificationRepository.save(sensorNotification);
+    }
+
+    @Override
+    public Sensor findSensorById(String sensorId) {
+        return sensorRepository.findById(sensorId).orElseThrow(() ->
+                new SensorDoesNotExistException(sensorId));
+    }
+
+    @Override
+    // find all sensors owned by user
+    public List<Sensor> findSensorsByUser(User user) {
+        return sensorRepository.findSensorByUser(user);
     }
 }
