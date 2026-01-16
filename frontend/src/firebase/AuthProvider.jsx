e3fbf8cf8dab64e911e3019ee8f0b870b16ceb15
Steven <pcmrsteven@gmail.com>
2026-01-16 19:37:47 +1100

    - fix invalid hook call by moving auth token out of axios interceptor (let react handle token instead)

--- frontend/src/firebase/AuthProvider.jsx
+++ frontend/src/firebase/AuthProvider.jsx
@@ -1,7 +1,8 @@
 import {useContext, createContext, useState, useEffect} from "react";
 import {useNavigate} from "react-router-dom";
 import {auth} from "./Firebase";
-import { onAuthStateChanged } from 'firebase/auth';
+import {onAuthStateChanged} from 'firebase/auth';
+import {setAuthToken} from '../api/axios.jsx';
 
 const AuthContext = createContext();
 
@@ -9,13 +10,22 @@ export function AuthProvider({children}) {
     const [user, setUser] = useState(null);
 
     useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
             setUser(user);
+
+            if (user) {
+                const token = await user.getIdToken();
+                setAuthToken(token);
+            } else {
+                setAuthToken(null);
+            }
         })
+
+        return () => unsubscribe();
     }, [])
 
     return (
-        <AuthContext.Provider value={ {user} }>
+        <AuthContext.Provider value={{user}}>
             {children}
         </AuthContext.Provider>
 
