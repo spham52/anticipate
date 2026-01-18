0fa5f27bf28f1cc7fdba2e058df524e49a48fd97
Steven <pcmrsteven@gmail.com>
2026-01-18 16:31:18 +1100

    - firebase must initialise immediately when service worker is created NOTE: it is safe to expose Firebase FCM API key. Just not the one used for the backend.

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -1,19 +1,22 @@
-importScripts(
-    "https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js"
-);
-importScripts(
-    "https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js"
-);
+/* eslint-disable */
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-fetch("/firebase-config.json")
-    .then((response) => {
-        return response.json();
-    })
-    .then((jsContent) => {
-        const config = eval(jsContent);
-        firebase.initializeApp(config.firebaseConfig);
-        firebase.messaging();
-    })
-    .catch((error) => {
-        console.error("Error initializing Firebase in service worker:", error);
+// must initialise service worker synchronously, not fetch. otherwise notifications won't work
+firebase.initializeApp({
+    apiKey: "AIzaSyDv9cuev1eXLtZu8EPCbhZTIIFQY3pZop0",
+    authDomain: "anticipateproject.firebaseapp.com",
+    projectId: "anticipateproject",
+    storageBucket: "anticipateproject.firebasestorage.app",
+    messagingSenderId: "497065267754",
+    appId: "1:497065267754:web:c9053e96b848701958a953",
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    self.registration.showNotification(payload.notification.title, {
+        body: payload.notification.body,
+        icon: '/favicon.ico'
     });
+});
