82d36dca9ce1a4078035febe7c07e1e19168e98b
CooperP1309 <cooperpeek1309@gmail.com>
2026-01-19 21:07:25 +1100

    layed out client tcp logic

--- pico/src/main.c
+++ pico/src/main.c
@@ -55,7 +55,45 @@ int main() {
         }
 
         printf("Connected to WiFi!\n");
-    }
+
+        // ATTEMPT AT SENDING PACKET TO SERVER
+
+
+        // DECLARE NEW TCP PCB
+        struct tcp_pcb *client_pcb = tcp_new_ip_type(IPADDR_TYPE_ANY);
+        if (!pcb) {
+            printf("[pico_captive_portal] error allocating pcb memory\n");
+            return -1;
+        }
+
+        // bind web port 80 to the pcb struct
+        int port = 80; // TODO: change port as needed (not necessary as client)
+        if (tcp_bind(pcb, IP_ANY_TYPE, port) < 0) {
+            printf("[pico_captive_portal] failed to bind port 80 to socket\n");
+            return -1;
+        }
+
+        ip_addr_t server_ip;
+        IP4_ADDR(&server_ip, 192, 168, 1, 100); // TODO: change to server's IP address
+        u16_t server_port = 8080;               
+
+        err_t err = tcp_connect(client_pcb, &server_ip, server_port, tcp_client_connected);
+        if (err != ERR_OK) {
+            // Handle error
+        }
+
+        // HANDLE TCP EVENTS IN CALLBACK FUNCTIONS AS FOLLOWS:
+        /**
+         * TCP connected callback: tcp_client_connected()
+         * TCP sent callback: tcp_client_sent()
+         * TCP recv callback: tcp_client_recv()
+         * TCP error callback: tcp_client_err()
+         * TCP poll callback: tcp_client_poll()
+         * TCP closed callback: tcp_client_close()
+         */
+
+        // send data via tcp_write() in tcp_client_connected() callback
+        
 
     return 0;
 }
