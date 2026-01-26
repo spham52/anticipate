edb1ac3b534685b02c3c4b6a503f0c10e1c9b3b6
CooperP1309 <cooperpeek1309@gmail.com>
2026-01-26 20:24:59 +1100

    made test functions in HAL library

--- pico/src/sensor_hal.c
+++ pico/src/sensor_hal.c
@@ -7,14 +7,6 @@
 #include "sensor_hal.h"
 
 void sensor_hal_init() {
-    // initialize stdio
-    stdio_init_all();
-
-    // initialize cyw43 wifi chip
-    if (cyw43_arch_init()) {
-        printf("[sensor_hal] failed to initialize cyw43_arch\n");
-        return;
-    }
 
     // initialize PIR sensor pin
     gpio_init(PIR_PIN);
@@ -23,4 +15,15 @@ void sensor_hal_init() {
     // initialize onboard LED pin
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+}
+
+void sensor_hal_poll() {
+
+    if (gpio_get(PIR_PIN)) {
+        printf("Motion Detected!\n");
+        gpio_put(LED_PIN, 1); // LED on
+        sleep_ms(1000);       // Debounce/Hold time
+    }
+
+    gpio_put(LED_PIN, 0); // LED off
 }
