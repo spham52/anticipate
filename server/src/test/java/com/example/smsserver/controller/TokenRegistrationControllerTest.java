4c8bfba2e68e411943377d0a4b0b132d42cb4137
Steven <pcmrsteven@gmail.com>
2026-01-31 15:35:56 +1100

    - fix bugs and missing mocks in TokenRegistrationController

--- server/src/test/java/com/example/smsserver/controller/TokenRegistrationControllerTest.java
+++ server/src/test/java/com/example/smsserver/controller/TokenRegistrationControllerTest.java
@@ -1,23 +1,37 @@
 package com.example.smsserver.controller;
 
 import com.example.smsserver.dto.Token.TokenRegistrationRequestDTO;
+import com.example.smsserver.service.TokenRegistrationService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.firebase.auth.FirebaseAuth;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(TokenRegistrationController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class TokenRegistrationControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
-    ObjectMapper objectMapper;
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private TokenRegistrationService tokenRegistrationService;
+
+    @MockitoBean
+    private FirebaseAuth firebaseAuth;
 
     @Test
     void registerToken_shouldSaveToken() throws Exception {
@@ -28,8 +42,10 @@ class TokenRegistrationControllerTest {
                 .build();
 
         mockMvc.perform(post("/token/register")
-                        .contentType("application/json")
+                        .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(registrationRequest)))
                 .andExpect(status().isOk());
+
+        verify(tokenRegistrationService).registerToken(any(), any());
     }
 }
