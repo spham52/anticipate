c34c140ef236272f6100ed64e05b09e783916e74
Steven <pcmrsteven@gmail.com>
2026-01-31 15:20:24 +1100

    rename class to 'TokenRegistrationControllerTest'

--- server/src/test/java/com/example/smsserver/controller/TokenRegistrationControllerTest.java
+++ server/src/test/java/com/example/smsserver/controller/TokenRegistrationControllerTest.java
@@ -11,7 +11,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(TokenRegistrationController.class)
-class RegistrationControllerTest {
+class TokenRegistrationControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
