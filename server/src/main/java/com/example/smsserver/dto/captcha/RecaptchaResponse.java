3e004a00d9898dd6377bfd7cbb528b06f8875f34
Steven <pcmrsteven@gmail.com>
2026-02-04 19:36:06 +1100

    - add 'No-Args Constructor' annotation

--- server/src/main/java/com/example/smsserver/dto/captcha/RecaptchaResponse.java
+++ server/src/main/java/com/example/smsserver/dto/captcha/RecaptchaResponse.java
@@ -3,8 +3,10 @@ package com.example.smsserver.dto.captcha;
 // DTO for Google's Recaptcha response
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class RecaptchaResponse {
     private boolean success;
 }
